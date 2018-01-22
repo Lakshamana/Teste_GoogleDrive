75cd15e22088fa5fae3e32d73ea5c978e60647fc
Lakshamana <guitrompa1@gmail.com>
2018-01-22 10:20:01 -0300

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,12 @@
+package teste_googledrive;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Runner {
+    public static void main(String[] args) throws IOException,
+            URISyntaxException{
+        Teste t = new Teste();
+        t.run();
+    }
+}
